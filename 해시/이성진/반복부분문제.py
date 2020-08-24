f92bba2df06b2a1bf0a9b5182f28a243772869e8
IncredibleMonkey <42059680+IncredibleMonkey@users.noreply.github.com>
2020-08-25 01:59:12 +0900

    Update 반복부분문제.py

--- 해시/이성진/반복부분문제.py
+++ 해시/이성진/반복부분문제.py
@@ -1,19 +1,21 @@
-import sys
-sys.stdin=open("input.txt","rt")
-
 lenBuns=int(input())
 buns=input()
 
-maxBun=lenBuns
-while maxBun>2:
-    maxBun-=1
-    d={}
-    for i in range(lenBuns-maxBun+1):
-        tmp=buns[i:maxBun+i]
-        d[tmp]=d.get(tmp,0)+1
-        if d[tmp]==2:
-            print(len(tmp))
-            sys.exit()
+lt=0
+rt=lenBuns
+while lt<=rt:
+    mid=(lt+rt)//2
+    d=dict()
+    for i in range(lenBuns-mid+1):
+        tmp=buns[i:mid+i]
+        if tmp not in d.keys():
+            d[tmp]=1
+        else:
+            lt=mid+1  
+            break
+    else:
+        rt=mid-1
+if mid!=0:
+    print(mid)
 else:
     print(0)
-
