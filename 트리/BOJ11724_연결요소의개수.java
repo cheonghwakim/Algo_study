1f67a4b04b61a5e8b7be5d1467dfe29c28e6ae22
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-11-29 18:54:59 +0900

    Update BOJ11724_연결요소의개수.java

--- 트리/BOJ11724_연결요소의개수.java
+++ 트리/BOJ11724_연결요소의개수.java
@@ -3,10 +3,7 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class BOJ11724_연결요소의개수 {
