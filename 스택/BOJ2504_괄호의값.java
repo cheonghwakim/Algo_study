91a4b2927cdb31d66b90d2dbbcb8c361aa1d5351
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-12-01 00:11:52 +0900

    Add files via upload

--- 스택/BOJ2504_괄호의값.java
+++ 스택/BOJ2504_괄호의값.java
@@ -13,60 +13,71 @@ public class BOJ2504_괄호의값 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] gwalhoes = br.readLine().toCharArray();
 		
-		int value = 0;
+		int value;
 		for (int i = 0; i < gwalhoes.length; i++) {
-
+		value = 0;
 			if(gwalhoes[i] == '(' || gwalhoes[i] == '[')
 				stack.push(Character.toString(gwalhoes[i]));
 			
 			else if (gwalhoes[i] == ')') {
-//				stack.pop();
-//				stack.push("2");
-				
-				// 안에 만약 숫자가 있다면 숫자는 하나밖에 없을거야 그럼 *하면 됨
-				// 숫자 없으면 걍 2나 3 넣고
-				while(true) {
-					String tmp = stack.pop();
-					
-					if(tmp.equals("[")) {
-						System.out.println(0);
-						return;
-					} else if(tmp.equals("(")) {
-						stack.push("2");
-					} else {
-						value = Integer.parseInt(tmp)*2;
-						if(stack.pop().equals("("))
-							stack.push(String.valueOf(value));
-						else {
+				if(stack.peek().equals("(")) {
+					stack.pop();
+					stack.push("2");
+				}else {
+					// 안에 만약 숫자가 있다면 계속 더해
+					// 숫자 없으면 걍 2나 3 넣고
+					while(!stack.isEmpty()) {
+						String tmp = stack.pop();
+						
+						if(tmp.equals("[") || tmp.equals(")") || tmp.equals("]")) {
 							System.out.println(0);
 							return;
-						}	
+						} else if(tmp.equals("(")) {
+							stack.push(String.valueOf(value*2));
+							break;
+						}
+						value += Integer.parseInt(tmp);	
+					}
+					if(stack.isEmpty()) {
+						System.out.println(0);
+						return;
 					}
 				}
 			} else if (gwalhoes[i] == ']') {
-				while(true) {
-					String tmp = stack.pop();
-					
-					if(tmp.equals("(")) {
-						System.out.println(0);
-						return;
-					} else if(tmp.equals("[")) {
-						stack.push("3");
-					} else {
-						value = Integer.parseInt(tmp)*3;
-						if(stack.pop().equals("["))
-							stack.push(String.valueOf(value));
-						else {
+				if(stack.peek().equals("[")) {
+					stack.pop();
+					stack.push("3");
+				}else {
+					// 안에 만약 숫자가 있다면 계속 더해
+					// 숫자 없으면 걍 2나 3 넣고
+					while(!stack.isEmpty()) {
+						String tmp = stack.pop();
+						
+						if(tmp.equals("(") || tmp.equals(")") || tmp.equals("]")) {
 							System.out.println(0);
 							return;
-						}		
+						} else if(tmp.equals("[")) {
+							stack.push(String.valueOf(value*3));
+							break;
+						}
+						value += Integer.parseInt(tmp);	
+					}
+					if(stack.isEmpty()) {
+						System.out.println(0);
+						return;
 					}
 				}
 			}
 		}
 		
 		while(!stack.isEmpty()) {
-			answer += Integer.parseInt(stack.pop());
+			String pop = stack.pop();
+			
+			if(pop.equals("(") || pop.equals("[") || pop.equals(")") || pop.equals("]")){
+				System.out.println(0);
+				return;
+			}
+			answer += Integer.parseInt(pop);
 		}
 
 		System.out.println(answer);
