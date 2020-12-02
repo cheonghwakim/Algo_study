b18db465639a10b2de954739b9a27c07af7f9516
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-12-02 09:26:26 +0900

    Add files via upload

--- 큐/BOJ1202_보석도둑.java
+++ 큐/BOJ1202_보석도둑.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// 틀림 -> 
 public class BOJ1202_보석도둑 {
-	static int N, K;
+	static int N, K, answer;
 	static ArrayList<Integer> bags = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +18,17 @@ public class BOJ1202_보석도둑 {
 		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		PriorityQueue<Jwelry> pq = new PriorityQueue<>();
+		PriorityQueue<Jwelry> pq = new PriorityQueue<>(new Comparator<Jwelry>(){
+			@Override
+			public int compare(Jwelry o1, Jwelry o2) {
+				if(o1.weight != o2.weight)
+					return o1.weight - o2.weight;
+				else {
+					return o2.price - o1.price;
+				}
+				
+			}
+		});
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int weight = Integer.parseInt(st.nextToken());
@@ -26,46 +36,58 @@ public class BOJ1202_보석도둑 {
 			pq.add(new Jwelry(weight, price));
 		}
 		
+		
 		for (int i = 0; i < K; i++) {
 			int bag = Integer.parseInt(br.readLine());
 			bags.add(bag);
 		}
 		Collections.sort(bags);
 		
-		int result = 0;
-		while(!pq.isEmpty()) {
-			Jwelry jwel= pq.poll();
+//		while(!pq.isEmpty()) {
+//			Jwelry jwel= pq.poll();
+//			
+//			int size = bags.size();
+//			for (int i = 0; i < size; i++) {
+//				if(bags.get(i) >= jwel.weight) {
+//					result += jwel.price;
+//					bags.remove(i);
+//					break;
+//				}
+//			}	
+//			
+//			if(bags.size() == 0)
+//				break;
+//		}
+		
+		// 가격 내림차순
+		PriorityQueue<Jwelry> baginjwel = new PriorityQueue<>(new Comparator<Jwelry>(){
+			@Override
+			public int compare(Jwelry o1, Jwelry o2) {
+				return o2.price - o1.price;
+			}
+		});
+		for (int i = 0; i < bags.size(); i++) {
+			Jwelry cur = pq.poll();
 			
-			int size = bags.size();
-			for (int i = 0; i < size; i++) {
-				if(bags.get(i) >= jwel.weight) {
-					result += jwel.price;
-					bags.remove(i);
-					break;
-				}
-			}	
+			if(bags.get(i) >= cur.weight) {
+				baginjwel.add(cur);
+			}
 			
-			if(bags.size() == 0)
-				break;
+			if(!baginjwel.isEmpty()) {
+				answer += baginjwel.poll().price;
+			}
 		}
-		System.out.println(result);
+		
+		System.out.println(answer);
 	}
 	
-	static class Jwelry implements Comparable<Jwelry>{
+	static class Jwelry{
 		int weight;
 		int price;
 		
 		Jwelry(int weight, int price){
 			this.weight = weight;
 			this.price = price;
-		}
-
-		@Override
-		public int compareTo(Jwelry o) {
-			if(o.weight != this.weight)
-				return this.weight - o.weight;
-			else
-				return o.price - this.price;
 		}
 	}
 
