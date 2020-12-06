c0af22780dec30102bae336bdcf38931fb71f5ae
cheonghwakim <43662673+cheonghwakim@users.noreply.github.com>
2020-12-06 17:56:44 +0900

    Add files via upload

--- 문자열/BOJ9935_문자열폭발.java
+++ 문자열/BOJ9935_문자열폭발.java
@@ -9,22 +9,43 @@ public class BOJ9935_문자열폭발 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		string = br.readLine();
 		bomb = br.readLine();
 		
-		while(string.contains(bomb)) {
-			String result = "";
-			String[] tmp = string.split(bomb);
-			for (int i = 0; i < tmp.length; i++) {
-				result += tmp[i];
+		char[] result = new char[string.length()];
+		int idx = 0;
+		
+		for (int i = 0; i < string.length(); i++) {
+			result[idx++] = string.charAt(i);
+			
+			if(result[idx - 1] == bomb.charAt(bomb.length() - 1)) {
+				
+				if(idx < bomb.length()) continue;
+				
+				boolean flag = true;
+				for (int j = 0; j < bomb.length(); j++) {
+					if(result[idx - j - 1] != bomb.charAt(bomb.length() - j - 1)) { // 뒤에서부터 검사
+						flag = false;
+						break;
+					}
+				}					
+				if(flag)
+					idx -= bomb.length();
 			}
-			string = result;
 		}
 		
-		if(string.length() == 0)
-			System.out.println("FRULA");
-		else
-			System.out.println(string);
+		if(idx == 0)
+			sb.append("FRULA");
+		else {
+			for (int i = 0; i < idx; i++) {
+				sb.append(result[i]);
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+		
 	}
 
 }
