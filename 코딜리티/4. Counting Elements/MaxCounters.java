f0331b0ef158e2b776cb3aecb6c6c4ca35347d52
김청화 <cheonghwakim@github.com>
2021-07-14 08:49:06 +0900

    코딜리티 lesson4. MaxCounters / 효율성 주의!

--- 코딜리티/4. Counting Elements/MaxCounters.java
+++ 코딜리티/4. Counting Elements/MaxCounters.java
@@ -1,6 +1,5 @@
 // you can also use imports, for example:
 import java.util.*;
-
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
@@ -9,18 +8,24 @@ class Solution {
         // write your code in Java SE 8
 
         int[] answer = new int[N];
-        int max = 0;
+        int tmp = 0, max = 0;
         for(int i = 0; i < A.length; i++){
-            if (A[i] == N + 1)
-                Arrays.fill(answer, max);
-            else if(A[i] <= N) {
-                answer[A[i] - 1] += 1;
-                max = Math.max(answer[A[i] - 1], max);
+            if(A[i] <= N) {
+                answer[A[i] - 1] = Math.max(answer[A[i] - 1], max) + 1;
+                tmp = Math.max(tmp, answer[A[i] - 1]);
+            
+            } else if (A[i] == N + 1) {
+                max = tmp;
             }
         }
 
+        for(int i = 0; i < answer.length; i++){
+            if(answer[i] < max)
+                answer[i] = max;
+        }
         return answer;
     }
 }
 
 // 1차: https://app.codility.com/demo/results/trainingSMYN6U-T7G/ 효율성 통과 못함
+// 2차: https://app.codility.com/demo/results/trainingBBVYWU-QND/ 효율성 주의!
